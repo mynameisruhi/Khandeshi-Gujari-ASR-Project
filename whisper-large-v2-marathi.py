d85e0c0e2201d4adce3b625c92eb163269292bef
mynameisruhi <ruhipatilishere@gmail.com>
2026-07-13 14:55:13 -0500

    Create whisper-large-v2-marathi.py

---
+++ whisper-large-v2-marathi.py
@@ -0,0 +1,10 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("automatic-speech-recognition", model="DrishtiSharma/whisper-large-v2-marathi")
+
+# Load model directly
+from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
+
+processor = AutoProcessor.from_pretrained("DrishtiSharma/whisper-large-v2-marathi")
+model = AutoModelForSpeechSeq2Seq.from_pretrained("DrishtiSharma/whisper-large-v2-marathi")
